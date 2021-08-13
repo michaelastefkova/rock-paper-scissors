e629b12f567845adee17f1054bf84fe3e979ba91
Michaela Stefkova <37817290+michaelastefkova@users.noreply.github.com>
2021-08-13 16:57:45 +0100

    Create data.ts

---
+++ src/config/data.ts
@@ -0,0 +1,45 @@
+export const valuesRockPaperScissors = [
+	{
+		id: 1,
+		name: 'rock',
+		wins: [3]
+	},
+	{
+		id: 2,
+		name: 'paper',
+		wins: [1]
+	},
+	{
+		id: 3,
+		name: 'scissors',
+		wins: [2]
+	}
+];
+
+export const valuesRockPaperScissorsLizardSpock = [
+	{
+		id: 1,
+		name: 'rock',
+		wins: [3, 4]
+	},
+	{
+		id: 2,
+		name: 'paper',
+		wins: [1, 5]
+	},
+	{
+		id: 3,
+		name: 'scissors',
+		wins: [2, 4]
+	},
+	{
+		id: 4,
+		name: 'lizard',
+		wins: [2, 5]
+	},
+	{
+		id: 5,
+		name: 'spock',
+		wins: [1, 3]
+	}
+];
